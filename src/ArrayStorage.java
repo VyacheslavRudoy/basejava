f743d8d5ba67089f4376f1ddcc3b6f4767c1f5c6
Vyacheslav <social_smm@bk.ru>
2022-03-03 20:49:07 +0300

    add hw_1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,26 +5,56 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (Resume resume : storage) {
+            resume = null;
+        }
     }
 
     void save(Resume r) {
+        for (Resume resume : storage) {
+            if (resume.uuid == null) {
+                resume = r;
+                break;
+            }
+        }
     }
 
     Resume get(String uuid) {
+        for (Resume resume : getAll()) {
+            if (resume.uuid == uuid) {
+                return resume;
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (Resume resume : storage) {
+            if (resume.uuid == uuid) {
+                resume = null;
+                break;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] copyStorage = new Resume[storage.length];
+        System.arraycopy(storage, 0, copyStorage, 0, storage.length);
+        return copyStorage;
     }
 
     int size() {
-        return 0;
+        int size = 0;
+        for (Resume resume : storage) {
+            if (resume != null) {
+                size++;
+            } else {
+                break;
+            }
+        }
+        return size;
     }
 }
