fdd9c40f003cf58a67067e66c776e9c35c9b9195
Vyacheslav <social_smm@bk.ru>
2022-05-02 01:25:15 +0300

    made changes in hw_7

--- src/ru/javawebinar/basejava/ResumeTestData.java
+++ src/ru/javawebinar/basejava/ResumeTestData.java
@@ -103,17 +103,18 @@ public class ResumeTestData {
 
         List<Experience> experience = new ArrayList<>();
         experience.add(new Experience(LocalDate.of(2013, 10, 1), "Java Online Projects",
-                "Автор проекта.", "Создание, организация и проведение Java онлайн " +
+                "http://javaops.ru/", "Автор проекта.", "Создание, организация и " +
+                "проведение Java онлайн " +
                 "проектов и стажировок. \n"));
 
         experience.add(new Experience(LocalDate.of(2014, 10, 1), LocalDate.of(2016, 1,
-                1), "Wrike", "Старший разработчик (backend)",
+                1), "Wrike", "https://www.wrike.com/", "Старший разработчик (backend)",
                 "Проектирование и разработка онлайн платформы управления проектами Wrike " +
                         "(Java 8 API, Maven, Spring, MyBatis, Guava, Vaadin, PostgreSQL, Redis). Двухфакторная " +
                         "аутентификация, авторизация по OAuth1, OAuth2, JWT SSO. \n"));
 
         experience.add(new Experience(LocalDate.of(2012, 4, 1), LocalDate.of(2014, 10,
-                1), "RIT Center", "Java архитектор", "Организация " +
+                1), "RIT Center", null, "Java архитектор", "Организация " +
                 "процесса разработки системы ERP для разных окружений: релизная политика, версионирование, ведение" +
                 " CI (Jenkins), миграция базы (кастомизация Flyway), конфигурирование системы (pgBoucer, Nginx), " +
                 "AAA via SSO. Архитектура БД и серверной части системы. Разработка интергационных сервисов: CMIS," +
@@ -123,7 +124,7 @@ public class ResumeTestData {
                 " Bonita, Python scripting, Unix shell remote scripting via ssh tunnels, PL/Python \n"));
 
         experience.add(new Experience(LocalDate.of(2010, 12, 1), LocalDate.of(2012, 4,
-                1), "Luxoft (Deutsche Bank)", "Ведущий программист",
+                1), "Luxoft (Deutsche Bank)", "http://www.luxoft.ru/", "Ведущий программист",
                 "Участие в проекте Deutsche Bank CRM (WebLogic, Hibernate, Spring, Spring MVC, " +
                         "SmartGWT, GWT, Jasper, Oracle). Реализация клиентской и серверной части CRM. Реализация " +
                         "RIA-приложения для администрирования, мониторинга и анализа результатов в области " +
@@ -131,23 +132,25 @@ public class ResumeTestData {
                         " Commet, HTML5. \n"));
 
         experience.add(new Experience(LocalDate.of(2008, 6, 1), LocalDate.of(2010, 12,
-                1), "Yota", "Ведущий специалист", "Дизайн и " +
+                1), "Yota", "https://www.yota.ru/", "Ведущий специалист", "Дизайн и " +
                 "имплементация Java EE фреймворка для отдела \"Платежные Системы\" (GlassFish v2.1, v3, OC4J, EJB3," +
                 " JAX-WS RI 2.1, Servlet 2.4, JSP, JMX, JMS, Maven2). Реализация администрирования, статистики и " +
                 "мониторинга фреймворка. Разработка online JMX клиента (Python/ Jython, Django, ExtJS) \n"));
 
         experience.add(new Experience(LocalDate.of(2007, 3, 1), LocalDate.of(2008, 6,
-                1), "Enkata", "Разработчик ПО", "Реализация " +
-                "клиентской (Eclipse RCP) и серверной (JBoss 4.2, Hibernate 3.0, Tomcat, JMS) частей кластерного" +
-                " J2EE приложения (OLAP, Data mining). \n"));
+                1), "Enkata", "http://enkata.com/", "Разработчик ПО",
+                "Реализация клиентской (Eclipse RCP) и серверной (JBoss 4.2, Hibernate 3.0, " +
+                        "Tomcat, JMS) частей кластерного" +
+                        " J2EE приложения (OLAP, Data mining). \n"));
 
         experience.add(new Experience(LocalDate.of(2005, 1, 1), LocalDate.of(2007, 2,
-                1), "Siemens AG", "Разработчик ПО", "Разработка " +
-                "информационной модели, проектирование интерфейсов, реализация и отладка ПО на мобильной IN платформе" +
+                1), "Siemens AG", "https://www.siemens.com/ru/ru/home.html",
+                "Разработчик ПО", "Разработка информационной модели, проектирование " +
+                "интерфейсов, реализация и отладка ПО на мобильной IN платформе" +
                 " Siemens @vantage (Java, Unix). \n"));
 
         experience.add(new Experience(LocalDate.of(1997, 9, 1), LocalDate.of(2005, 1,
-                1), "Alcatel", "Инженер по аппаратному и программному тестированию",
+                1), "Alcatel", "http://www.alcatel.ru/", "Инженер по аппаратному и программному тестированию",
                 "Тестирование, отладка, внедрение ПО цифровой телефонной станции Alcatel 1000 S12" +
                         " (CHILL, ASM). \n"));
 
@@ -156,33 +159,35 @@ public class ResumeTestData {
 
         List<Experience> education = new ArrayList<>();
         education.add(new Experience(LocalDate.of(2013, 3, 1), LocalDate.of(2013, 5,
-                1), "Coursera", "'Functional Programming Principles in Scala'" +
-                " by Martin Odersky", "\n"));
+                1), "Coursera", "https://www.coursera.org/course/progfun",
+                "'Functional Programming Principles in Scala'" +
+                        " by Martin Odersky", "\n"));
 
         education.add(new Experience(LocalDate.of(2011, 3, 1), LocalDate.of(2011, 4,
-                1), "Luxoft", "Курс 'Объектно-ориентированный анализ ИС. " +
-                "Концептуальное моделирование на UML.'", "\n"));
+                1), "Luxoft", "http://www.luxoft-training.ru/training/catalog/course.html?ID=22366",
+                "Курс 'Объектно-ориентированный анализ ИС. " +
+                        "Концептуальное моделирование на UML.'", "\n"));
 
         education.add(new Experience(LocalDate.of(2005, 1, 1), LocalDate.of(2005, 4,
-                1), "Siemens AG", "3 месяца обучения мобильным IN сетям " +
+                1), "Siemens AG", "http://www.siemens.ru/", "3 месяца обучения мобильным IN сетям " +
                 "(Берлин)", "\n"));
 
         education.add(new Experience(LocalDate.of(1997, 9, 1), LocalDate.of(1998, 3,
-                1), "Alcatel", "6 месяцев обучения цифровым телефонным " +
+                1), "Alcatel", "http://www.alcatel.ru/", "6 месяцев обучения цифровым телефонным " +
                 "сетям (Москва)", "\n"));
 
         education.add(new Experience(LocalDate.of(1993, 9, 1), LocalDate.of(1996, 7,
                 1), "Санкт-Петербургский национальный исследовательский университет " +
-                "информационных технологий, механики и оптики", "Аспирантура (программист С, С++)",
+                "информационных технологий, механики и оптики", "http://www.ifmo.ru/", "Аспирантура (программист С, С++)",
                 "\n"));
 
         education.add(new Experience(LocalDate.of(1987, 9, 1), LocalDate.of(1993, 7,
                 1), "Санкт-Петербургский национальный исследовательский университет " +
-                "информационных технологий, механики и оптики", "Инженер (программист Fortran, C)",
+                "информационных технологий, механики и оптики", "http://www.ifmo.ru/", "Инженер (программист Fortran, C)",
                 "\n"));
 
         education.add(new Experience(LocalDate.of(1984, 9, 1), LocalDate.of(1987, 6,
-                1), "Заочная физико-техническая школа при МФТИ", "Закончил" +
+                1), "Заочная физико-техническая школа при МФТИ", "http://www.school.mipt.ru/", "Закончил" +
                 " с отличием", "\n"));
 
         section.put(SectionType.EDUCATION, new ExperienceSection(education));
--- src/ru/javawebinar/basejava/model/Experience.java
+++ src/ru/javawebinar/basejava/model/Experience.java
@@ -1,45 +1,70 @@
 package ru.javawebinar.basejava.model;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 public class Experience {
+    private final Link homePage;
+
     private final LocalDate startDate;
     private final LocalDate finishDate;
-    private final String companyName;
     private final String positionName;
     private final String additionalInformation;
 
-    public Experience(LocalDate startDate, LocalDate finishDate, String companyName, String positionName, String additionalInformation) {
+    public Experience(LocalDate startDate, LocalDate finishDate, String companyName, String url, String positionName, String additionalInformation) {
+        Objects.requireNonNull(startDate, "startDate must not be null");
+        Objects.requireNonNull(finishDate, "endDate must not be null");
+        Objects.requireNonNull(positionName, "positionName must not be null");
         this.startDate = startDate;
         this.finishDate = finishDate;
-        this.companyName = companyName;
+        this.homePage = new Link(companyName, url);
         this.positionName = positionName;
         this.additionalInformation = additionalInformation;
     }
 
-    public Experience(LocalDate startDate, String companyName, String positionName, String additionalInformation) {
+    public Experience(LocalDate startDate, String companyName, String url, String positionName, String additionalInformation) {
+        Objects.requireNonNull(startDate, "startDate must not be null");
+        Objects.requireNonNull(positionName, "positionName must not be null");
         this.startDate = startDate;
         this.finishDate = null;
-        this.companyName = companyName;
+        this.homePage = new Link(companyName, url);
         this.positionName = positionName;
         this.additionalInformation = additionalInformation;
-    }
-
-    public String replacementFinishDate(LocalDate finishDate) {
-        if (finishDate == null) {
-            return "Сейчас";
-        }
-        return String.valueOf(finishDate);
     }
 
     @Override
     public String toString() {
+        String copyFinishDate = finishDate == null ? "Сейчас" : String.valueOf(finishDate);
         return "Experience{" +
-                "startDate='" + startDate + '\'' +
-                ", finishDate=" + replacementFinishDate(finishDate) +
-                ", companyName='" + companyName + '\'' +
+                "homePage=" + homePage +
+                ", startDate=" + startDate +
+                ", finishDate=" + copyFinishDate +
                 ", positionName='" + positionName + '\'' +
                 ", additionalInformation='" + additionalInformation + '\'' +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Experience that = (Experience) o;
+
+        if (!homePage.equals(that.homePage)) return false;
+        if (!startDate.equals(that.startDate)) return false;
+        if (finishDate != null ? !finishDate.equals(that.finishDate) : that.finishDate != null) return false;
+        if (!positionName.equals(that.positionName)) return false;
+        return additionalInformation != null ? additionalInformation.equals(that.additionalInformation) : that.additionalInformation == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = homePage.hashCode();
+        result = 31 * result + startDate.hashCode();
+        result = 31 * result + (finishDate != null ? finishDate.hashCode() : 0);
+        result = 31 * result + positionName.hashCode();
+        result = 31 * result + (additionalInformation != null ? additionalInformation.hashCode() : 0);
+        return result;
     }
 }
