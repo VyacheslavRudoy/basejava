18f425d69339562c99a5fe7c03cbee954cda209c
Vyacheslav <social_smm@bk.ru>
2022-08-02 00:17:29 +0300

    HW lesson_10

--- src/ru/javawebinar/basejava/model/Organization.java
+++ src/ru/javawebinar/basejava/model/Organization.java
@@ -63,6 +63,14 @@ public class Organization implements Serializable {
                 '}';
     }
 
+    public Link getHomePage() {
+        return homePage;
+    }
+
+    public List<Position> getPositions() {
+        return positions;
+    }
+
 
     @XmlAccessorType(XmlAccessType.FIELD)
     public static class Position implements Serializable {
--- src/ru/javawebinar/basejava/storage/serializer/DataStreamSerializer.java
+++ src/ru/javawebinar/basejava/storage/serializer/DataStreamSerializer.java
@@ -1,9 +1,11 @@
 package ru.javawebinar.basejava.storage.serializer;
 
-import ru.javawebinar.basejava.model.ContactType;
-import ru.javawebinar.basejava.model.Resume;
+import ru.javawebinar.basejava.model.*;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class DataStreamSerializer implements StreamSerializer {
@@ -19,7 +21,31 @@ public class DataStreamSerializer implements StreamSerializer {
                 dos.writeUTF(entry.getKey().name());
                 dos.writeUTF(entry.getValue());
             }
-            // TODO implements sections
+
+            Map<SectionType, Section> sections = r.getSection();
+            dos.writeInt(sections.size());
+            for (Map.Entry<SectionType, Section> entry : sections.entrySet()) {
+                SectionType sectionType = entry.getKey();
+                Section section = entry.getValue();
+                dos.writeUTF(sectionType.name());
+                switch (sectionType) {
+                    case OBJECTIVE:
+                    case PERSONAL:
+                        dos.writeInt(sectionType.getTitle().length());
+                        dos.writeUTF(((TextSection) section).getText());
+                        break;
+                    case ACHIEVEMENT:
+                    case QUALIFICATIONS:
+                        dos.writeInt(sectionType.getTitle().length());
+                        dos.writeUTF((((ListSection) section).getList()).toString());
+                        break;
+                    case EXPERIENCE:
+                    case EDUCATION:
+                        dos.writeInt(sectionType.getTitle().length());
+                        dos.writeUTF((((OrganizationSection) section).getExperiences()).toString());
+                        break;
+                }
+            }
         }
     }
 
@@ -29,12 +55,50 @@ public class DataStreamSerializer implements StreamSerializer {
             String uuid = dis.readUTF();
             String fullName = dis.readUTF();
             Resume resume = new Resume(uuid, fullName);
-            int size = dis.readInt();
-            for (int i = 0; i < size; i++) {
+            int sizeContacts = dis.readInt();
+            for (int i = 0; i < sizeContacts; i++) {
                 resume.addContacts(ContactType.valueOf(dis.readUTF()), dis.readUTF());
             }
-            // TODO implements sections
+            int sizeSectionType = dis.readInt();
+            for (int i = 0; i < sizeSectionType; i++) {
+                SectionType sectionType = SectionType.valueOf(dis.readUTF());
+                Map<SectionType, Section> section = new HashMap<>();
+                int sizeObjective = dis.readInt();
+                for (int a = 0; a < sizeObjective; a++) {
+                    section.put(SectionType.OBJECTIVE, new TextSection(dis.readUTF()));
+                }
+                int sizePersonal = dis.readInt();
+                for (int b = 0; b < sizePersonal; b++) {
+                    section.put(SectionType.PERSONAL, new TextSection(dis.readUTF()));
+                }
+                int sizeAchievement = dis.readInt();
+                for (int c = 0; c < sizeAchievement; c++) {
+                    ArrayList<String> achievements = new ArrayList<>();
+                    achievements.add(dis.readUTF());
+                    section.put(SectionType.ACHIEVEMENT, new ListSection(achievements));
+                }
+                int sizeQualifications = dis.readInt();
+                for (int d = 0; d < sizeQualifications; d++) {
+                    ArrayList<String> qualifications = new ArrayList<>();
+                    qualifications.add(dis.readUTF());
+                    section.put(SectionType.QUALIFICATIONS, new ListSection(qualifications));
+                }
+                int sizeExperience = dis.readInt();
+                for (int e = 0; e < sizeExperience; e++) {
+                    List<String> experience = new ArrayList<>();
+                    experience.add(dis.readUTF());
+                    section.put(SectionType.EXPERIENCE, new ListSection(experience));
+                }
+                int sizeEducation = dis.readInt();
+                for (int f = 0; f < sizeEducation; f++) {
+                    List<String> experience = new ArrayList<>();
+                    experience.add(dis.readUTF());
+                    section.put(SectionType.EXPERIENCE, new ListSection(experience));
+                }
+                resume.setSection(section);
+            }
             return resume;
         }
     }
 }
+
