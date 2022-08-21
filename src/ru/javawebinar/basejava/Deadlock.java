0a3b50e75a4a52f3d480543aff023bdfbc45a1bc
Vyacheslav <social_smm@bk.ru>
2022-08-21 13:33:34 +0300

    made changes in hw_11

--- src/ru/javawebinar/basejava/Deadlock.java
+++ src/ru/javawebinar/basejava/Deadlock.java
@@ -13,36 +13,30 @@ public class Deadlock {
 
     private static class ThreadDemo1 extends Thread {
         public void run() {
-            synchronized (Lock1) {
-                System.out.println("Thread 1: Holding lock 1...");
-
-                try {
-                    Thread.sleep(10);
-                } catch (InterruptedException e) {
-                }
-                System.out.println("Thread 1: Waiting for lock 2...");
-
-                synchronized (Lock2) {
-                    System.out.println("Thread 1: Holding lock 1 and 2...");
-                }
-            }
+            String nameThread = "Thread 1";
+            Deadlock.closingThread(Lock1, Lock2, nameThread);
         }
     }
 
     private static class ThreadDemo2 extends Thread {
         public void run() {
-            synchronized (Lock2) {
-                System.out.println("Thread 2: Holding lock 2...");
+            String nameThread = "Thread 2";
+            Deadlock.closingThread(Lock2, Lock1, nameThread);
+        }
+    }
 
-                try {
-                    Thread.sleep(10);
-                } catch (InterruptedException e) {
-                }
-                System.out.println("Thread 2: Waiting for lock 1...");
+    public static void closingThread(Object lock1, Object lock2, String nameThread) {
+        synchronized (lock1) {
+            System.out.println(nameThread + " : Holding " + nameThread + "...");
 
-                synchronized (Lock1) {
-                    System.out.println("Thread 2: Holding lock 1 and 2...");
-                }
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+            }
+            System.out.println(nameThread + " : Waiting for lock others thread...");
+
+            synchronized (lock2) {
+                System.out.println(nameThread + " : Holding lock 1 and 2...");
             }
         }
     }
