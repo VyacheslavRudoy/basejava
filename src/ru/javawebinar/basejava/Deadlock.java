85da1339ad9d831fd09686f633f8ecd646ed14d4
Vyacheslav <social_smm@bk.ru>
2022-08-22 12:58:35 +0300

    made changes in hw_11

--- src/ru/javawebinar/basejava/Deadlock.java
+++ src/ru/javawebinar/basejava/Deadlock.java
@@ -5,39 +5,37 @@ public class Deadlock {
     public static Object Lock2 = new Object();
 
     public static void main(String args[]) {
-        ThreadDemo1 T1 = new ThreadDemo1();
-        ThreadDemo2 T2 = new ThreadDemo2();
-        T1.start();
-        T2.start();
-    }
-
-    private static class ThreadDemo1 extends Thread {
-        public void run() {
-            String nameThread = "Thread 1";
-            Deadlock.closingThread(Lock1, Lock2, nameThread);
+        String nameThread1 = "Thread 1";
+        String nameThread2 = "Thread 2";
+        Deadlock.closingThread(Lock1, Lock2, nameThread1);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
-    }
-
-    private static class ThreadDemo2 extends Thread {
-        public void run() {
-            String nameThread = "Thread 2";
-            Deadlock.closingThread(Lock2, Lock1, nameThread);
-        }
+        Deadlock.closingThread(Lock2, Lock1, nameThread2);
     }
 
     public static void closingThread(Object lock1, Object lock2, String nameThread) {
-        synchronized (lock1) {
-            System.out.println(nameThread + " : Holding " + nameThread + "...");
+        Runnable run = new Runnable() {
+            @Override
+            public void run() {
+                synchronized (lock1) {
+                    System.out.println(nameThread + " : Holding " + nameThread + "...");
 
-            try {
-                Thread.sleep(10);
-            } catch (InterruptedException e) {
-            }
-            System.out.println(nameThread + " : Waiting for lock others thread...");
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                    }
+                    System.out.println(nameThread + " : Waiting for lock others thread...");
 
-            synchronized (lock2) {
-                System.out.println(nameThread + " : Holding lock 1 and 2...");
+                    synchronized (lock2) {
+                        System.out.println(nameThread + " : Holding lock 1 and 2...");
+                    }
+                }
             }
-        }
+        };
+        Thread thread = new Thread(run);
+        thread.start();
     }
 }
